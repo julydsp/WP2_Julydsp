ba9d2c63b64fbf5d2d3b2d212fc2d29ee19c89d6
julydsp <julydsp04@gmail.com>
2022-09-23 14:27:40 +0700

    memodifikasi

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -2,12 +2,12 @@
 
 class Contoh1 extends CI_Controller
 {
-public function index()
-{
-echo "<h1>Perkenalkan</h1>";
-echo"Nama saya JULY DWI SAPUTRA
-Saya tinggal di daerah SUKOHARJO
-olah raga yang saya sukai adalah
-Bulutangkis";
+    public function index()
+        {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya JULY DWI SAPUTRA
+        Saya tinggal di daerah SUKOHARJO
+        olah raga yang saya sukai adalah
+        Bulutangkis";
 }
 }
