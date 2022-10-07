0229c9698279ab23354ec40623db15db2c9a4829
julydsp <julydsp04@gmail.com>
2022-10-07 15:46:28 +0700

    MENAMBAH ABOUT

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>11111
+    <h4>Nama</h4>
+        <ul type="disc">
+            <li>Nama Depan : JULY</li>
+            <li>Nama Belakang : DWI SAPUTRA</li>
+        </ul>
+        <br>
+        <h4>Alamat</h4>
+        <ul type="none">
+            <li> Tompe Rt.04/02,Lorog,Tawangsari</li>
+        </ul>
+        <h4>Tempat Lahir</h4>
+        <ul type="none">
+            <li>SUKOHARJO</li>
+        </ul>
+        <h4>Olah Raga Favorit</h4>
+        <ul type="square">
+            <li>Tiudur</li> 
+            <li>Game</li>
+        </ul>
+</section>
